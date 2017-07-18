08e710b59011b311356ecf45f5a176c660fc1fb8
Oleksandr Malakhov <alexever17@gmail.com>
2017-07-18 18:53:14 +0200

    x

--- master.js
+++ master.js
@@ -8,5 +8,5 @@ var mapProp= {
     zoom:7,
 };
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+};
 //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
