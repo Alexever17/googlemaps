30db3258fcedfb8f83f4cf8e4d9efe3f19ad098a
Oleksandr Malakhov <alexever17@gmail.com>
2017-07-18 16:56:51 +0200

    1

--- master.js
+++ master.js
@@ -1,6 +1,10 @@
+
+var lat = 51.508742;
+var lng = -0.120850;
+
 function myMap() {
 var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
+    center:new google.maps.LatLng(lat,lng),
     zoom:5,
 };
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
